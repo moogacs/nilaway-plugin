5c7d883d5f27bbf47c2114afd92c86cd80d2a71a
Taimoor Ahmad <taimoor@opslevel.com>
2024-03-03 18:08:48 -0500

    add requested changes

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 YELLOW=$'\e[0;33m'
 NC=$'\e[0m'
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"
+
+[[ -z "${GITHUB_WORKSPACE:-}" ]] && echo "\$GITHUB_WORKSPACE is not set." && exit 1
+
 [[ -z "${1:-}" ]] && echo "usage: $0 (v1.X.X|latest)" && exit 1
 
 gomod=$(go list -m -json github.com/golangci/golangci-lint@"$1" | grep -o '"GoMod": "[^"]*' | grep -o '[^"]*$')
@@ -13,14 +18,17 @@ tools_version=$(grep 'golang.org/x/tools' <<< "$output" | grep -oE '\bv[0-9]+\.[
 go_version=$(grep -oE '^go [0-9]+\.[0-9].+' <<< "$output" | cut -d ' ' -f2)
 
 local_go_version=$(go version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-[[ "$local_go_version" != "$go_version" ]] && \
-  echo -e "${YELLOW}warning: local go version ($local_go_version) != go version in go.mod from golangci-lint@$1  ($go_version)${NC}" &&
-  echo -e "${YELLOW}continuing build like normal ...${NC}"
+if [[ "$local_go_version" != "$go_version" ]]; then
+  echo "${YELLOW}warning: local go version ($local_go_version) != go version in go.mod from golangci-lint@$1  ($go_version)${NC}"
+  echo "continuing build like normal ..."
+fi
+
+[[ -f "go.mod" || -f "go.sum" ]] && rm -f go.*
 
 go mod init github.com/nilaway-plugin
 go mod tidy
 go mod edit -replace golang.org/x/tools=golang.org/x/tools@v"$tools_version"
 go mod tidy
 
-go build -o "$GITHUB_WORKSPACE/.plugins/nilaway.so"  -buildmode=plugin -trimpath plugin/nilaway.go && \
-  echo -e "nilaway.so is built compatible with golangci-lint $1."
+go build -o "$GITHUB_WORKSPACE/.plugins/nilaway.so"  -buildmode=plugin -trimpath plugin/nilaway.go
+echo "nilaway.so is built compatible with golangci-lint@$1."
--- local_build.sh
+++ local_build.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 YELLOW=$'\e[0;33m'
 NC=$'\e[0m'
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"
+
 output=$(go version -m "$(which golangci-lint)")
 GOARCH=$(grep -oE 'GOARCH=\S+' <<< "$output" | cut -d '=' -f 2)
 GOOS=$(grep -oE 'GOOS=\S+' <<< "$output" | cut -d '=' -f 2)
@@ -12,17 +15,21 @@ tools_version=$(grep 'golang.org/x/tools' <<< "$output" | grep -oE '\bv[0-9]+\.[
 go_version=$(grep -oE 'go[0-9]+\.[0-9]+\.[0-9]+' <<< "$output" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
 
 local_go_version=$(go version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-[[ "$local_go_version" != "$go_version" ]] && \
-  echo -e "${YELLOW}warning: local go version ($local_go_version) != go version used to build $(which golangci-lint) ($go_version)${NC}" &&
-  echo -e "${YELLOW}continuing build like normal ...${NC}"
+if [[ "$local_go_version" != "$go_version" ]]; then
+  echo "${YELLOW}warning: local go version ($local_go_version) != go version used to build $(which golangci-lint) ($go_version)${NC}"
+  echo "continuing build like normal ..."
+fi
 
-echo "$tools_version"
+[[ -f "go.mod" || -f "go.sum" ]] && rm -f go.*
+
+set -x
 go mod init github.com/nilaway-plugin
 go mod tidy
 go mod edit -replace golang.org/x/tools=golang.org/x/tools@v"$tools_version"
 go mod tidy
 
-echo building nilaway.so with GOARCH="$GOARCH" GOOS="$GOOS" tools_version="$tools_version" local_go_version="$local_go_version" ...
-GOARCH=$GOARCH GOOS=$GOOS go build -buildmode=plugin -trimpath -o nilaway.so ./plugin/nilaway.go && \
-  echo -e "nilaway.so is built compatible with local golangci-lint, please move it under your project directory" \
-    "and make sure to copy the custom settings to your .golangci.yml file."
+GOARCH=$GOARCH GOOS=$GOOS go build -buildmode=plugin -trimpath -o nilaway.so ./plugin/nilaway.go
+set +x
+
+echo "nilaway.so is built compatible with local golangci-lint, please move it under your project directory" \
+  "and make sure to copy the custom settings to your .golangci.yml file."
