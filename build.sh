5224e823a32b0b6f581cd33c789be95e0e1d268f
Taimoor Ahmad <taimoor@opslevel.com>
2024-03-03 21:01:29 -0500

    combine build scripts

--- build.sh
+++ build.sh
@@ -2,33 +2,87 @@
 
 set -euo pipefail
 
+CYAN=$'\e[0;36m'
+GREEN=$'\e[0;32m'
 YELLOW=$'\e[0;33m'
 NC=$'\e[0m'
 
+# script-wide vars
+build_out="nilaway.so"
+slug=""
+
+[[ -n "${GITHUB_WORKSPACE:-}" ]] && build_out="$GITHUB_WORKSPACE/.plugins/nilaway.so"
+
+# set directory, parse CLI args
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
-[[ -z "${GITHUB_WORKSPACE:-}" ]] && echo "\$GITHUB_WORKSPACE is not set." && exit 1
+usage() {
+  echo "usage: $0 VERSION"
+  echo
+  echo "examples:"
+  echo "  on your local machine/to build for the golangci-lint in your \$PATH:"
+  echo "    $0 local"
+  echo
+  echo "  in ci/to build for a specific golangci-lint version or latest:"
+  echo "    $0 (v1.X.X|latest)"
+  echo
+  echo "  if \$GITHUB_WORKSPACE is set, the .so files will be output in \$GITHUB_WORKSPACE/.plugins/"
+  echo "    otherwise, they will be output in $SCRIPT_DIR/"
+  exit 1
+}
 
-[[ -z "${1:-}" ]] && echo "usage: $0 (v1.X.X|latest)" && exit 1
+case "${1:-}" in
+  local)
+    slug="$(which golangci-lint)"
+    ;;
+  latest|v[0-9]*)
+    slug="golangci-lint@$1"
+    ;;
+  *)
+    usage
+    ;;
+esac
+echo "${CYAN}building nilaway plugin to be compatible with: ${slug}${NC}"
 
-gomod=$(go list -m -json github.com/golangci/golangci-lint@"$1" | grep -o '"GoMod": "[^"]*' | grep -o '[^"]*$')
-output=$(cat "$gomod")
-tools_version=$(grep 'golang.org/x/tools' <<< "$output" | grep -oE '\bv[0-9]+\.[0-9]+\.[0-9]+\b' | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-go_version=$(grep -oE '^go [0-9]+\.[0-9].+' <<< "$output" | cut -d ' ' -f2)
+# gather info about golangci-lint version the plugin is being built for
+# set: tools_version, go_version
+# also set: GOARCH, GOOS -- only if they are not set already.
+if [[ "$1" == "local" ]]; then
+  output=$(go version -m "$(which golangci-lint)")
+  tools_version=$(grep 'golang.org/x/tools' <<< "$output" | grep -oE '\bv[0-9]+\.[0-9]+\.[0-9]+\b' | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+  go_version=$(grep -oE 'go[0-9]+\.[0-9]+\.[0-9]+' <<< "$output" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
 
-local_go_version=$(go version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-if [[ "$local_go_version" != "$go_version" ]]; then
-  echo -e "${YELLOW}warning: local go version ($local_go_version) != go version in go.mod from golangci-lint@$1  ($go_version)${NC}"
-  echo -e "continuing build like normal ..."
+  # go version should provide info about GOARCH/GOOS, set it
+  [[ -z "${GOARCH:-}" ]] && GOARCH=$(grep -oE 'GOARCH=\S+' <<< "$output" | cut -d '=' -f 2) && echo "${YELLOW}\$GOARCH was empty, now set to ${GOARCH}${NC}"
+  [[ -z "${GOOS:-}" ]] && GOOS=$(grep -oE 'GOOS=\S+' <<< "$output" | cut -d '=' -f 2) && echo "${YELLOW}\$GOOS was empty, now set to ${GOOS}${NC}"
+else
+  gomod=$(go list -m -json github.com/golangci/golangci-lint@"$1" | grep -o '"GoMod": "[^"]*' | grep -o '[^"]*$')
+  output=$(cat "$gomod")
+  tools_version=$(grep 'golang.org/x/tools' <<< "$output" | grep -oE '\bv[0-9]+\.[0-9]+\.[0-9]+\b' | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+  go_version=$(grep -oE '^go [0-9]+\.[0-9].+' <<< "$output" | cut -d ' ' -f2)
+
+  # go.mod does not give any GOARCH/GOOS info, so just use what is in go env
+  [[ -z "${GOARCH:-}" ]] && GOARCH=$(go env | grep GOARCH | sed s/\'//g | cut -d '=' -f 2) && echo "${YELLOW}\$GOARCH was empty, now set to ${GOARCH}${NC}"
+  [[ -z "${GOOS:-}" ]] && GOOS=$(go env | grep GOOS | sed s/\'//g | cut -d '=' -f 2) && echo "${YELLOW}\$GOOS was empty, now set to ${GOOS}${NC}"
 fi
 
-[[ -f "go.mod" || -f "go.sum" ]] && rm -f go.*
+# different go versions could potentially cause a problem, inform user
+local_go_version=$(go version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+if [[ "$local_go_version" != "$go_version 1" ]]; then
+  echo "${YELLOW}warning: local go version ($local_go_version) != go version for $slug ($go_version), this could potentially cause problems${NC}"
+  echo "continuing build like normal ..."
+fi
 
+# perform build
+[[ -f "go.mod" || -f "go.sum" ]] && rm -f go.*
+set -x
 go mod init github.com/nilaway-plugin
-go mod tidy
 go mod edit -replace golang.org/x/tools=golang.org/x/tools@v"$tools_version"
 go mod tidy
 
-go build -o "$GITHUB_WORKSPACE/.plugins/nilaway.so"  -buildmode=plugin -trimpath plugin/nilaway.go
-echo "nilaway.so is built compatible with golangci-lint@$1."
+GOARCH=$GOARCH GOOS=$GOOS go build -o "$build_out"  -buildmode=plugin -trimpath plugin/nilaway.go
+
+set +x
+echo "${GREEN}nilaway.so is built compatible with local golangci-lint, please move it under your project directory" \
+  "and make sure to copy the custom settings to your .golangci.yml file.${NC}"
--- local_build.sh
+++ local_build.sh
@@ -2,34 +2,7 @@
 
 set -euo pipefail
 
-YELLOW=$'\e[0;33m'
-NC=$'\e[0m'
+echo "DEPRECATED - use instead:"
+echo "  ./build.sh local"
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$SCRIPT_DIR"
-
-output=$(go version -m "$(which golangci-lint)")
-GOARCH=$(grep -oE 'GOARCH=\S+' <<< "$output" | cut -d '=' -f 2)
-GOOS=$(grep -oE 'GOOS=\S+' <<< "$output" | cut -d '=' -f 2)
-tools_version=$(grep 'golang.org/x/tools' <<< "$output" | grep -oE '\bv[0-9]+\.[0-9]+\.[0-9]+\b' | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-go_version=$(grep -oE 'go[0-9]+\.[0-9]+\.[0-9]+' <<< "$output" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-
-local_go_version=$(go version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-if [[ "$local_go_version" != "$go_version" ]]; then
-  echo -e "${YELLOW}warning: local go version ($local_go_version) != go version used to build $(which golangci-lint) ($go_version)${NC}"
-  echo -e "continuing build like normal ..."
-fi
-
-[[ -f "go.mod" || -f "go.sum" ]] && rm -f go.*
-
-set -x
-go mod init github.com/nilaway-plugin
-go mod tidy
-go mod edit -replace golang.org/x/tools=golang.org/x/tools@v"$tools_version"
-go mod tidy
-
-GOARCH=$GOARCH GOOS=$GOOS go build -buildmode=plugin -trimpath -o nilaway.so ./plugin/nilaway.go
-set +x
-
-echo "nilaway.so is built compatible with local golangci-lint, please move it under your project directory" \
-  "and make sure to copy the custom settings to your .golangci.yml file."
+exit 1
